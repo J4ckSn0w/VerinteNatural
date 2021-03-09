d32b47b01a5fbc4bdbcce129d38e437149fb8839
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-03-09 12:24:50 -0600

    Addresses tables was created

---
+++ backend/database/migrations/2021_03_09_174135_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('street', 255);
+            $table->string('number', 40);
+            $table->string('zip_code', 40);
+            $table->bigInteger('customer_id')->unsigned();
+
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
