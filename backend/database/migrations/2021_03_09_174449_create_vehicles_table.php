062538a1b1950c825fc167d93b2e1f3f957ffb1e
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-03-09 12:26:12 -0600

    Vehicles tables was created

---
+++ backend/database/migrations/2021_03_09_174449_create_vehicles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('license_plate', 255)->nullable();
+            $table->string('brand', 100)->nullable();
+            $table->string('description', 255)->nullable();
+            $table->unsignedBigInteger('vehicle_type_id');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('vehicle_type_id')->references('id')->on('vehicle_types');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
