8318f2ac3f339f6601e1aa2cd96caead0cef8aa7
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-03-13 10:39:58 -0600

    Request to update customer was finished

---
+++ backend/app/Http/Requests/CustomerUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'      => 'required|string|max:255',
+            'phone_number' => 'required|string|min:5|max:50|unique:users,phone_number,' . $this->id,
+            'rfc'   => ['required','string', 'regex:/^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1]))((-)?([A-Z\d]{3}))?$/']
+        ];
+    }
+}
