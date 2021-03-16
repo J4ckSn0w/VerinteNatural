c48295b9c18b110172a900e75da318912adc4bcb
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-03-16 08:27:58 -0600

    No used methods was removed

--- backend/app/Http/Controllers/AddressController.php
+++ backend/app/Http/Controllers/AddressController.php
@@ -29,16 +29,6 @@ class AddressController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -56,17 +46,6 @@ class AddressController extends Controller
      * @return Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
     {
         //
     }
