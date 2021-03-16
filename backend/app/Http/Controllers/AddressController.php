79f761b3156cfce9a797c30b62bbc96e1effe791
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-03-16 09:06:59 -0600

    Error returned was added on try-catch block

--- backend/app/Http/Controllers/AddressController.php
+++ backend/app/Http/Controllers/AddressController.php
@@ -23,7 +23,7 @@ class AddressController extends Controller
             $addresses = Address::where('customer_id', Auth::user()->customer->id)->get();
             return response()->json(['data' => $addresses], 200);
         } catch (\Exception $e) {
-            return response()->json(['error' => 'No fue posible realizar la consulta'], 401);
+            return response()->json(['error' => $e->getMessage()], 401);
         }
 
 
