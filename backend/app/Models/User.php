64a7a4ab08baf313bbd696249a30de5bcfaa3a36
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-04-05 10:13:08 -0500

    Boot method was added on some models to implement log functionally and log Model was created

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use Laravel\Passport\HasApiTokens;
 
