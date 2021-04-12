6a04d24694542d5ef4182172f0866c53bb267802
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-04-12 13:09:55 -0500

    Tables to product and product types was created

---
+++ backend/database/migrations/2021_04_12_164021_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->unsignedBigInteger('product_type_id')->nullable();
+            $table->string('description', 255)->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('product_type_id')->references('id')->on('product_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
