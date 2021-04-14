ad62f2c4a0e7cc283e804c4de77067e3f72ebf7b
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-04-14 10:54:48 -0500

    Providers tables was created

---
+++ backend/database/migrations/2021_04_14_134429_create_providers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProvidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('providers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->string('contact', 255)->nullable();
+            $table->string('address', 255)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->string('phone_number', 100)->nullable();
+            $table->string('schedule', 255)->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('providers');
+    }
+}
