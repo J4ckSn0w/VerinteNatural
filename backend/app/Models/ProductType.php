7f43538701f1a119f4708daf37cc7034ac4846e1
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-04-14 10:52:37 -0500

    Soft delete method was added on some models and Provider model was created

--- backend/app/Models/ProductType.php
+++ backend/app/Models/ProductType.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Auth;
 
 class ProductType extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'product_types';
 
