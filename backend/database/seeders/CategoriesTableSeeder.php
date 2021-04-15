f5268833ccd00527d8a3309e085d4716ec34e88f
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-04-15 09:52:45 -0500

    Seeder to categories table was created

---
+++ backend/database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            'id' => 1,
+            'name' => 'Frutas',
+            'code' => 'FR',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table('categories')->insert([
+            'id' => 2,
+            'name' => 'Verduras',
+            'code' => 'VE',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table('categories')->insert([
+            'id' => 3,
+            'name' => 'Cereales',
+            'code' => 'CE',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table('categories')->insert([
+            'id' => 4,
+            'name' => 'Legumbres',
+            'code' => 'LE',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
