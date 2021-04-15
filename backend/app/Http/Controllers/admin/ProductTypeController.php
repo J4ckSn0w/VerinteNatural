fba1d1d069ff988420facac09692cee7d89b2b5d
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-04-15 09:41:42 -0500

    Product type controller was modified

--- backend/app/Http/Controllers/admin/ProductTypeController.php
+++ backend/app/Http/Controllers/admin/ProductTypeController.php
@@ -16,7 +16,27 @@ class ProductTypeController extends Controller
     public function index()
     {
         try {
-            return response()->json(['data' => ProductType::all()], 200);
+            $category = request()->get('category', false);
+
+            if ($category)
+                $product_types = ProductType::where('category_id', $category)->get();
+            else
+                $product_types = ProductType::all();
+
+            $product_types  = $product_types->map(function ($product_type) {
+                $product_type->append(['category_name']);
+                $product_type = $product_type->only([
+                    'id',
+                    'name',
+                    'code',
+                    'category_name',
+                    'category_id',
+                    'created_at'
+                ]);
+                return $product_type;
+            });
+
+            return response()->json(['data' => $product_types], 200);
         } catch (\Exception $e) {
             return response()->json(['error' => ['errors' => ['server_error' => $e->getMessage()]]], 400);
         }
@@ -31,8 +51,20 @@ class ProductTypeController extends Controller
     public function store(ProductTypeRequest $request)
     {
         try {
-            $product_type = ProductType::create($request->all());
-            return response()->json(['data' => $product_type], 201);
+            $name = $request->name;
+            $data = array_merge(
+                $request->toArray(),
+                ['code' => strtoupper($name[0]) .
+                    strtoupper($name[strlen($name) - 1]) .
+                    (strlen($name) < 10 ? '0' . strlen($name) : strlen($name))]
+            );
+
+            if (!ProductType::where('code', $data['code'])->first()) {
+                $product_type = ProductType::create($data);
+                return response()->json(['data' => $product_type], 201);
+            } else {
+                return response()->json(['error' => ['errors' => ['name' => 'No se puede obtener un código valido con este nombre']]], 400);
+            }
         } catch (\Exception $e) {
             return response()->json(['error' => ['errors' => ['server_error' => $e->getMessage()]]], 400);
         }
@@ -64,7 +96,8 @@ class ProductTypeController extends Controller
     {
         try {
             $product_type = ProductType::find($id);
-            $product_type->fill($request->all());
+            $product_type->name = $request->name;
+            $product_type->category_id = $request->category_id;
             $product_type->save();
             return response()->json(['data' => $product_type], 200);
         } catch (\Exception $e) {
