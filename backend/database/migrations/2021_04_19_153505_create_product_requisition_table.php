e3c5871111d9ca1699e6e415b9bbde4b529ae6eb
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-04-19 12:46:06 -0500

    Table to save products of requisition was finished

---
+++ backend/database/migrations/2021_04_19_153505_create_product_requisition_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductRequisitionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_requisition', function (Blueprint $table) {
+            $table->unsignedBigInteger('requisition_id')->nullable();
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->double('quantity')->nullable();
+
+            $table->foreign('requisition_id')->references('id')->on('requisitions');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_requisition');
+    }
+}
