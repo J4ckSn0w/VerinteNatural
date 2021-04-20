4d2d86ae6893e06a56aea96ec60da42984d14642
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-04-20 17:19:49 -0500

    Methods to download vehicle was created

--- backend/app/Http/Controllers/admin/VehicleController.php
+++ backend/app/Http/Controllers/admin/VehicleController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\VehicleRequest;
+use App\Models\MileageRecord;
 use App\Models\Vehicle;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use PDF;
 
 class VehicleController extends Controller
 {
@@ -19,7 +22,7 @@ class VehicleController extends Controller
     {
         try {
             return response()->json(['data' => Vehicle::all()], 200);
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 400);
         }
     }
@@ -35,7 +38,7 @@ class VehicleController extends Controller
         try {
             $vehicle = Vehicle::create($request->all());
             return response()->json(['data' => $vehicle], 200);
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 400);
         }
     }
@@ -50,7 +53,7 @@ class VehicleController extends Controller
     {
         try {
             return response()->json(['data' => Vehicle::findOrfail($id)->load('vehicle_type')], 200);
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 400);
         }
     }
@@ -62,14 +65,14 @@ class VehicleController extends Controller
      * @param int $id
      * @return JsonResponse
      */
-    public function update(Request $request,int $id): JsonResponse
+    public function update(VehicleRequest $request, int $id): JsonResponse
     {
         try {
             $vehicle = Vehicle::findOrfail($id);
             $vehicle->fill($request->all());
             $vehicle->save();
             return response()->json(['data' => $vehicle], 200);
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 400);
         }
     }
@@ -86,8 +89,39 @@ class VehicleController extends Controller
             $vehicle = Vehicle::findOrfail($id);
             $vehicle->delete();
             return response()->json(['data' => $vehicle], 200);
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    public function vehicleReport($id, $date_from, $date_to)
+    {
+        try {
+            $vehicle = Vehicle::findOrfail($id)->append('vehicle_type_name');
+            $from = new Carbon($date_from);
+            $to = new Carbon($date_to);
+
+            $mileages = MileageRecord::where('vehicle_id', $id)->whereBetween('created_at', [$from, $to])->get();
+            $pdf = PDF::loadView('pdfs.vehicleReport', compact('vehicle', 'mileages', 'date_to', 'date_from'));
+            return $pdf->download('reporte_vehiculo_' . $vehicle->license_plate . '__' . Carbon::now()->format('Ymd') . '.pdf'); //view('pdfs.vehicleReport', compact('vehicle', 'mileages', 'date_to', 'date_from')); 
+        } catch (\Exception $e) {
+            return response()->json(['error' => ['errors' => ['server_error' => $e->getMessage()]]], 400);
+        }
+    }
+
+    public function vehicleReportGeneral()
+    {
+        try {
+            $vehicles = Vehicle::all();
+            $vehicles = $vehicles->map(function ($vehicle) {
+                $vehicle->append('vehicle_type_name');
+                return $vehicle;
+            });
+            $date = Carbon::now()->format('d-m-Y');
+            $pdf = PDF::loadView('pdfs.vehiclesReport', compact('vehicles', 'date'));
+            return $pdf->download('reporte_general_vehicular' . Carbon::now()->format('Ymd') . '.pdf');
+        } catch (\Exception $e) {
+            return response()->json(['error' => ['errors' => ['server_error' => $e->getMessage()]]], 400);
         }
     }
 }
