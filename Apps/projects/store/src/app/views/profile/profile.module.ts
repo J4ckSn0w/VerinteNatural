bca90b1d2698a4dc476f5b4d66b2e36bf5da6561
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-04-24 10:29:24 -0500

    Routing module was modified

--- Apps/projects/store/src/app/views/profile/profile.module.ts
+++ Apps/projects/store/src/app/views/profile/profile.module.ts
@@ -1,11 +1,27 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ProfileRoutingModule } from 'views/profile/profile.routing';
+import { CommonModule } from '@angular/common'
+
+import { ProfileComponent } from 'views/profile/profile.component'
+
+import { ProfileRoutingModule } from 'views/profile/profile-routing.module';
+import { AddressesComponent } from 'components/profile/addresses/addresses.component'
+import { MyOrdersComponent } from 'components/profile/my-orders/my-orders.component'
+import { MyPendingsOrdersComponent } from 'components/profile/my-pendings-orders/my-pendings-orders.component'
+import { UserDataComponent } from 'components/profile/user-data/user-data.component'
+import { ChangePasswordComponent } from 'components/profile/change-password/change-password.component'
 
 @NgModule({
     imports: [
         CommonModule,
-        ProfileRoutingModule
+        ProfileRoutingModule,
+    ],
+    declarations: [
+        ProfileComponent,
+        AddressesComponent,
+        MyOrdersComponent,
+        MyPendingsOrdersComponent,
+        UserDataComponent,
+        ChangePasswordComponent,
     ]
 })
 export class ProfileModule { }
