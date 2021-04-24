c579f987cd8c50dcfe2916d97a2bb5283be2c9d1
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-04-24 10:18:05 -0500

    Routing was modified

--- Apps/projects/store/src/app/app.routing.ts
+++ Apps/projects/store/src/app/app.routing.ts
@@ -1,12 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { HomeComponent } from 'views/home/home.component';
-import { ProfileModule } from 'views/profile/profile.module';
 //Views
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'profile', loadChildren: () => ProfileModule }
+    { path: 'profile', loadChildren: () => import('./views/profile/profile.module').then(m => m.ProfileModule) }
 ]
 
 export const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes, {
