cb064bcb39196cccde09d49a165d2e2df3b992ba
Fernando Acevedo <facevedoperes@gmail.com>
2021-04-26 17:21:42 -0500

    Update 26/04/2021

--- backend/database/migrations/2021_04_13_134956_create_bouncer_tables.php
+++ backend/database/migrations/2021_04_13_134956_create_bouncer_tables.php
@@ -22,8 +22,8 @@ class CreateBouncerTables extends Migration
             $table->bigInteger('entity_id')->unsigned()->nullable();
             $table->string('entity_type')->nullable();
             $table->boolean('only_owned')->default(false);
-            $table->json('options')->nullable();
-            //$table->integer('scope')->nullable()->index();
+            $table->text('options')->nullable();
+            $table->integer('scope')->nullable()->index();
             $table->timestamps();
         });
 
