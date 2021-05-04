abf7f5eb5045682e29df840a46364dfc8b262c13
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2021-05-04 12:23:05 -0500

    Abilities table seeder was modified

--- backend/database/seeders/AbilitiesAndRoles.php
+++ backend/database/seeders/AbilitiesAndRoles.php
@@ -49,61 +49,222 @@ class AbilitiesAndRoles extends Seeder
             'orders_edit'
         ];
 
+        $purchase_orders = [
+            'purchase_orders_show',
+            'purchase_orders_complete',
+            'purchase_orders_create',
+            'purchase_orders_delete',
+            'purchase_orders_index',
+            'purchase_orders_edit'
+        ];
 
         //
 
-        // Admin Role
-        $admin = Bouncer::role()->firstOrCreate([
-            'name' => 'admin',
-            'title' => 'Administrador',
-            'employee_type_id' => 1
-        ]);
 
-        // Assign Admin role to user with ID 1
-        Bouncer::assign($admin)->to(User::find(1));
-
-        // Abilities of role
-        $this->addAbilitiesToRole(
-            [
-                'role' => $admin,
-                'abilities_modules' => [$warehouses, $employees, $products, $orders]
-            ]
-        );
-
-        // Warehouse leader
+        // Admin
         $this->addAbilitiesToRole(
             [
                 'role' => Bouncer::role()->firstOrCreate([
-                    'name' => 'warehouse_leader',
-                    'title' => 'Encargado de almácen',
-                    'employee_type_id' => 2
+                    'name' => 'admin',
+                    'title' => 'Administrador',
+                    'employee_type_id' => 1,
+                    'level' => 1,
+                    'section' => 1
+                ]),
+                'abilities_modules' => [$warehouses, $employees, $products, $orders, $purchase_orders]
+            ]
+        );
+
+
+        // Warehouse
+        // Warehouse Manager
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'warehouse_manager',
+                    'title' => 'Jefe de almácen',
+                    'employee_type_id' => 2,
+                    'level' => 2,
+                    'section' => 1
+                ]),
+                'abilities_modules' => [$warehouses]
+            ]
+        );
+        // Warehouse employee
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'warehouse_employee',
+                    'title' => 'Almacenista',
+                    'employee_type_id' => 2,
+                    'level' => 3,
+                    'section' => 1
                 ]),
                 'abilities_modules' => [$warehouses]
             ]
         );
 
 
-        // Warehouse leader
+        // Sales
+        // Sales Manager
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'sales_manager',
+                    'title' => 'Jefe de ventas',
+                    'employee_type_id' => 3,
+                    'level' => 2,
+                    'section' => 1
+                ]),
+                'abilities_modules' => [$products, $orders]
+            ]
+        );
+        // Custom service supervisor
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'customer_service_supervisor',
+                    'title' => 'Supervisor de servicio al cliente',
+                    'employee_type_id' => 3,
+                    'level' => 3,
+                    'section' => 2
+                ]),
+                'abilities_modules' => [$products, $orders]
+            ]
+        );
+        // Delivery
         $this->addAbilitiesToRole(
             [
                 'role' => Bouncer::role()->firstOrCreate([
                     'name' => 'delivery',
                     'title' => 'Repartidor',
-                    'employee_type_id' => 3
+                    'employee_type_id' => 3,
+                    'level' => 4,
+                    'section' => 2
                 ]),
-                'abilities_modules' => [['orders_index', 'orders_show', 'orders_complete']]
+                'abilities_modules' => [$products, $orders]
             ]
         );
-
-        // Warehouse leader
+        // Custom service employee
         $this->addAbilitiesToRole(
             [
                 'role' => Bouncer::role()->firstOrCreate([
-                    'name' => 'gatherer',
-                    'title' => 'Recolector',
-                    'employee_type_id' => 3
+                    'name' => 'customer_service_employee',
+                    'title' => 'Ejecutivo de sevicio al cliente',
+                    'employee_type_id' => 3,
+                    'level' => 4,
+                    'section' => 2
                 ]),
-                'abilities_modules' => []
+                'abilities_modules' => [$products, $orders]
+            ]
+        );
+        // Reception
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'reception',
+                    'title' => 'Recepción',
+                    'employee_type_id' => 3,
+                    'level' => 3,
+                    'section' => 3
+                ]),
+                'abilities_modules' => [$products, $orders]
+            ]
+        );
+        // Sales supervisor
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'sales_supervisor',
+                    'title' => 'Supervidor de ventas',
+                    'employee_type_id' => 3,
+                    'level' => 3,
+                    'section' => 4
+                ]),
+                'abilities_modules' => [$products, $orders]
+            ]
+        );
+        // Sales employee
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'sales_employee',
+                    'title' => 'Agente de ventas',
+                    'employee_type_id' => 3,
+                    'level' => 4,
+                    'section' => 4
+                ]),
+                'abilities_modules' => [$products, $orders]
+            ]
+        );
+        // After sales supervisor
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'after_sales_supervisor',
+                    'title' => 'Supervisor de post-ventas',
+                    'employee_type_id' => 3,
+                    'level' => 3,
+                    'section' => 5
+                ]),
+                'abilities_modules' => [$products, $orders]
+            ]
+        );
+        // After sales employee
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'after_sales_employee',
+                    'title' => 'Agente de post-ventas',
+                    'employee_type_id' => 3,
+                    'level' => 4,
+                    'section' => 5
+                ]),
+                'abilities_modules' => [$products, $orders]
+            ]
+        );
+
+
+
+
+
+        // Purchasing Manager
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'purchasing_manager',
+                    'title' => 'Jefe de compras',
+                    'employee_type_id' => 4,
+                    'level' => 2,
+                    'section' => 1
+                ]),
+                'abilities_modules' => [$purchase_orders]
+            ]
+        );
+        // Purchasing Employee
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'purchasing_employee',
+                    'title' => 'Agente de compras',
+                    'employee_type_id' => 4,
+                    'level' => 3,
+                    'section' => 2
+                ]),
+                'abilities_modules' => [$purchase_orders]
+            ]
+        );
+        // Purchasing Employee
+        $this->addAbilitiesToRole(
+            [
+                'role' => Bouncer::role()->firstOrCreate([
+                    'name' => 'purchasing_gatherer',
+                    'title' => 'Recolector',
+                    'employee_type_id' => 4,
+                    'level' => 3,
+                    'section' => 2
+                ]),
+                'abilities_modules' => [$purchase_orders]
             ]
         );
     }
